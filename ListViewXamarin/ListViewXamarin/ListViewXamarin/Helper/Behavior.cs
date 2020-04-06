7558cc2c1af9d2a8622f30f99f1117cb6cc3fe5c
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-06 15:52:39 +0530

    XAMARIN-34035 - Sample and screen shot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<SfListView>
+    {
+        SfListView listView;
+        protected override void OnAttachedTo(SfListView bindable)
+        {
+            listView = bindable;
+            listView.SelectionChanging += ListView_SelectionChanging;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void ListView_SelectionChanging(object sender, ItemSelectionChangingEventArgs e)
+        {
+            if (listView.SelectionMode == Syncfusion.ListView.XForms.SelectionMode.Single)
+            {
+                if (e.AddedItems.Count > 0)
+                {
+                    var item = e.AddedItems[0] as Contacts;
+                    item.LabelTextColor = Color.Red;
+                }
+                if (e.RemovedItems.Count > 0)
+                {
+                    var item = e.RemovedItems[0] as Contacts;
+                    item.LabelTextColor = Color.Black;
+                }
+            }
+        }
+
+        protected override void OnDetachingFrom(SfListView bindable)
+        {
+            listView.SelectionChanging -= ListView_SelectionChanging;
+
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
